c1f5a39b45e1fa3d8119c1ef8100b3b3cbaeb928
Lucas Engel <lucasengel@gmail.com>
2020-11-16 12:58:20 -0500

    Updated resume information

--- src/config/resume-experiences.js
+++ src/config/resume-experiences.js
@@ -5,16 +5,14 @@ export const experience = [
     start: "feb 2018",
     end: "sept 2020",
     description: [
-      "Launched the new Drupal-powered college main website",
-      "Wrote technical WCAG 2.0 requirements for AA compliance",
-      "Automated processes using NodeJS",
+      "Implemented Pardot integrations with Salesforce and Drupal CMS",
+      "Integral to the launched the College’s new Drupal-powered website, achieved under pandemic conditions",
+      "Scoped and documented technical and business requirements for a range of new system builds and inte-grations",
+      "Planned and executed seamless data migration from old .NET CMS to new PHP powered CMS",
+      "Automated processes using NodeJS that cut down team effort up to two weeks",
       "Built sites and prototypes in Vue.js and React",
-      "Conducted the data migration from old to new CMS",
-      "Lead the QA’ing process of the new website and CMS",
-      "Modelled data structures for website components and program management software integration",
-      "Implemented Pardot integrations with Salesforce and the new website",
-      "Scoped the new website technical and business requirements",
-      "Created the decision criteria matrix for the college’s new CMS",
+      "Lead quality assurance testing and remediation processes",
+      "Modelled data structures for website and software integration",
     ],
   },
   {
@@ -23,9 +21,9 @@ export const experience = [
     start: "oct 2016",
     end: "jul 2017",
     description: [
-      "Fully implemented a professional automation system, Accelo, on a team of 50+",
-      "Launched BDO branches and Vena Solutions websites and several Shopify ecommerces",
-      "Integrated email marketing automation tools for clients",
+      "End-to-end implementation of a professional automation system, Accelo, for a team of 50+",
+      "Launched websites for BMO branches, Vena Solutions and several Shopify ecommerce sites",
+      "Integrated email marketing automation tools",
     ],
   },
   {
@@ -41,9 +39,9 @@ export const experience = [
     start: "feb 2014",
     end: "sept 2015",
     description: [
-      "Built two versions of a web-based Learning Management Systems, on WordPress then Ruby on Rails",
+      "Implemented behaviour tracking, multi-variant testing and analytics tools",
+      "Built multiple versions of a web-based Learning Management Systems, on WordPress and on Ruby on Rails",
       "Optimized pages to better capture organic traffic through SEO",
-      "Implemented behaviour tracking, split testing (A/B) and analytics tools",
     ],
   },
   {
@@ -52,8 +50,8 @@ export const experience = [
     start: "apr 2013",
     end: "may 2014",
     description: [
-      "Technical Business Requirements",
-      "Coordinated development, QA and deployment teams",
+      "Scoped and documented technical business requirements for Brazil's largest cloth retailer ecommerce and marketing sites",
+      "Coordinated work of 20+ people on development, quality assurance and deployment teams",
     ],
   },
   {
@@ -63,7 +61,7 @@ export const experience = [
     end: "mar 2013",
     description: [
       "Frontend development using Ruby’s ERB and jQuery",
-      "Project management",
+      "Managed projects for several startups and Brazil's second largest tourism operator on a team of 7",
     ],
   },
 ]
@@ -81,7 +79,7 @@ export const education = [
     title: "B.A. Communications, Advertising",
     start: "jul 2007",
     end: "jun 2011",
-    institution: "Univerdiade Federal do Rio Grande do Sul",
+    institution: "Universidade Federal do Rio Grande do Sul",
   },
 ]
 
