ad84d2a3139b7751b47bc56b67d5fd23cc3b57a5
Lucas Engel <lucasengel@gmail.com>
2021-03-07 17:00:15 -0500

    Updated experiences

--- src/config/resume-experiences.js
+++ src/config/resume-experiences.js
@@ -1,25 +1,39 @@
 export const experience = [
   {
-    title: "Web Developer",
-    institution: "George Brown College",
-    start: "nov 2017",
-    end: "sept 2020",
+    title: "Frontend Software Engineer",
+    institution: "Yaar Inc",
+    start: "Dec 2020",
+    end: "...",
     description: [
-      "Integral to the launched the College’s new Drupal-powered website, achieved under pandemic conditions",
-      "Scoped and documented technical and business requirements for a range of new system builds and inte-grations",
-      "Planned and executed seamless data migration from old .NET CMS to new PHP powered CMS",
-      "Automated processes using NodeJS that cut down team effort up to two weeks",
-      "Built sites and prototypes in Vue.js and React",
-      "Lead quality assurance testing and remediation processes",
-      "Modelled data structures for website and software integration",
+      "Working on the development of Yaar's iOS React Native, Redux, Typescript app",
+      "Adding to and improving on the components library",
+      "Converting entire codebase to Typescript",
+      "Implementing Machine Learning workflows in the frontend",
+      "Contributing to a rapid prototyping workflow",
+      "End-to-end design integration",
+    ],
+  },
+  {
+    title: "Frontend Web Developer",
+    institution: "George Brown College",
+    start: "Nov 2017",
+    end: "Sept 2020",
+    description: [
+      "Built web components and prototypes in React and Vue.js",
+      "Automated processes using NodeJS that significantly cut down team effort",
       "Implemented Pardot integrations with Salesforce and Drupal CMS",
+      "Integral to the launched the College’s new Drupal-powered website, achieved under pandemic conditions",
+      "Scoped and documented technical and business requirements for a range of new system builds and integrations",
+      "Planned and executed seamless data migration from old .NET CMS to new PHP powered CMS",
+      "Lead quality assurance testing and remediation processes",
+      "Modelled data structures for website and software integrations",
     ],
   },
   {
     title: "Technical Project Manager",
     institution: "TygerShark Inc",
-    start: "oct 2016",
-    end: "jul 2017",
+    start: "Oct 2016",
+    end: "Jul 2017",
     description: [
       "End-to-end implementation of a professional automation system, Accelo, for a team of 50+",
       "Launched websites for BMO branches, Canadian Tire, Vena Solutions and several Shopify ecommerce sites",
@@ -29,21 +43,21 @@ export const experience = [
     ],
   },
   {
-    title: "Web developer",
+    title: "Frontend Web Developer",
     institution: "Chef Bruno",
-    start: "oct 2015",
-    end: "sept 2016",
+    start: "Oct 2015",
+    end: "Sept 2016",
     description: [
-      "Built multiple revisions of Sketch designs in a collaboration with the design team",
-      "Frontend development in AngularJS, HTML, SASS",
+      "Frontend development in AngularJS, HTML, Sass",
       "Used D3.js for graphics, Bootstrap as CSS framework, Grunt as a build tool",
+      "Built multiple revisions of Sketch designs in a collaboration with the design team",
     ],
   },
   {
     title: "Co-Founder",
     institution: "Auditorium LMS",
-    start: "feb 2014",
-    end: "sept 2015",
+    start: "Feb 2014",
+    end: "Sept 2015",
     description: [
       "Built multiple versions of a web-based Learning Management Systems, on WordPress and on Ruby on Rails",
       "Implemented behaviour tracking, multi-variant testing and analytics tools",
@@ -53,8 +67,8 @@ export const experience = [
   {
     title: "Business Analyst",
     institution: "Compasso",
-    start: "apr 2013",
-    end: "may 2014",
+    start: "Apr 2013",
+    end: "May 2014",
     description: [
       "Scoped and documented technical business requirements for Brazil's largest clothing retailer ecommerce and marketing sites",
       "Coordinated work of 20+ people on development, quality assurance and deployment teams",
@@ -63,8 +77,8 @@ export const experience = [
   {
     title: "Co-Founder",
     institution: "Fande Web Creation",
-    start: "jan 2009",
-    end: "mar 2013",
+    start: "Jan 2009",
+    end: "Mar 2013",
     description: [
       "Frontend development using Ruby’s ERB and jQuery",
       "Managed projects for several startups and Brazil's second largest tourism operator on a team of 7",
@@ -75,16 +89,16 @@ export const experience = [
 export const education = [
   {
     title: "GoStack Bootcamp",
-    start: "sept 2020",
-    end: "nov 2020",
+    start: "Sept 2020",
+    end: "Nov 2020",
     institution: "RocketSeat",
     description:
-      "Two-month full-time bootcamp on React, React Native and Node. Built 3 APIs, 4 React sites, and 3 React Native apps",
+      "Two-month full-time bootcamp on React, React Native and Node. Built 3 APIs, 4 React sites, and 3 React Native apps. Course also covered Postgres, Redix, MongoDB databases and SOLID principles.",
   },
   {
     title: "B.A. Communications, Advertising",
-    start: "jul 2007",
-    end: "jun 2011",
+    start: "Jul 2007",
+    end: "Jun 2011",
     institution: "Universidade Federal do Rio Grande do Sul",
   },
 ]
--- src/config/resume-profile.js
+++ src/config/resume-profile.js
@@ -2,8 +2,8 @@ export const profile = {
   name: `Lucas Engel`,
   title: `Frontend Developer`,
   bio: [
-    `Frontend developer with experience in Project Management, Business Analysis, Marketing and Graphic Design.`,
-    `Specialized in React, TypeScript, Node.js.`,
+    `I have 3+ years of experience in frontend software development and 7 years in IT Project Management and Business Analysis.`,
+    `I’m currently working with mobile app development in React Native, Redux, Typeorm, and Typescript and have also worked with React, Vue.js and AngularJS.`,
   ],
   phone: `+1 (705) 970-6195`,
   email: `lucasengel@gmail.com`,
